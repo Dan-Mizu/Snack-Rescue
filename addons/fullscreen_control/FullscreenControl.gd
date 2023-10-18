5198521f55bbbce376e9b1473b1a7bfdea8459ba
Dan <dan@danmizu.dev>
2023-10-17 20:48:00 -0700

    Exclusive Fullscreen

--- addons/fullscreen_control/FullscreenControl.gd
+++ addons/fullscreen_control/FullscreenControl.gd
@@ -18,10 +18,10 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed(_toggle_fullscreen_action):
 			var current_mode = DisplayServer.window_get_mode()
-			if current_mode == DisplayServer.WINDOW_MODE_FULLSCREEN:
+			if current_mode == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
 				_update_fullscreen_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 			else:
-				_update_fullscreen_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+				_update_fullscreen_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
 
 func _update_fullscreen_mode(mode: DisplayServer.WindowMode):
 	_save_fullscreen_mode(mode)
